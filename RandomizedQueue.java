3d62c2c00aef1d91dcd143046c9d8874c953343e
Abyssalisk <Abyssalisk@gmail.com>
2018-09-29 19:12:48 -0600

    Added Iterator functionality, improved comments.

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,6 +1,9 @@
 package a02;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdRandom;
 
 /**
  * 
@@ -10,12 +13,17 @@ import java.util.Iterator;
  */
 public class RandomizedQueue<Item> implements Iterable<Item>
 {
+	private Item[] itemArray;
+	private int n;
+
 	/**
 	 * Constructs an empty randomized queue.
 	 */
+	@SuppressWarnings("unchecked")
 	public RandomizedQueue()
 	{
-
+		itemArray = (Item[]) new Object[2];
+		n = 0;
 	}
 
 	/**
@@ -25,7 +33,7 @@ public class RandomizedQueue<Item> implements Iterable<Item>
 	 */
 	public boolean isEmpty()
 	{
-		return false;// TODO
+		return n == 0;
 
 	}
 
@@ -36,7 +44,7 @@ public class RandomizedQueue<Item> implements Iterable<Item>
 	 */
 	public int size()
 	{
-		return 0;// TODO
+		return n;
 
 	}
 
@@ -47,7 +55,16 @@ public class RandomizedQueue<Item> implements Iterable<Item>
 	 */
 	public void enqueue(Item item)
 	{
+		if (itemArray == null)
+		{
+			throw new NullPointerException();
+		}
+		if (isStorageFull())
+		{
+			doubleSize();
+		}
 
+		itemArray[n++] = item;
 	}
 
 	/**
@@ -57,7 +74,24 @@ public class RandomizedQueue<Item> implements Iterable<Item>
 	 */
 	public Item dequeue()
 	{
-		return null;// TODO
+		if (isEmpty())
+		{
+			throw new NoSuchElementException();
+		}
+
+		int random = StdRandom.uniform(n);
+		Item returnItem = itemArray[random];
+		n--;
+
+		itemArray[random] = itemArray[n];
+		itemArray[n] = null;
+
+		if (isStorageOversized())
+		{
+			halveSize();
+		}
+
+		return returnItem;
 
 	}
 
@@ -68,7 +102,13 @@ public class RandomizedQueue<Item> implements Iterable<Item>
 	 */
 	public Item sample()
 	{
-		return null;// TODO
+		if (isEmpty())
+		{
+			throw new NoSuchElementException();
+		}
+
+		int random = StdRandom.uniform(n);
+		return itemArray[random];
 
 	}
 
@@ -77,13 +117,102 @@ public class RandomizedQueue<Item> implements Iterable<Item>
 	 */
 	public Iterator<Item> iterator()
 	{
-		return null;// TODO
+		return new RandomizedQueueIterator();
+	}
+
+	private class RandomizedQueueIterator implements Iterator<Item>
+	{
+		private Item[] iteratorArray;
+		private int length = n;
+
+		@Override
+		public boolean hasNext()
+		{
+			return n > 0;
+		}
+
+		@Override
+		public Item next()
+		{
+			if (!hasNext())
+			{
+				throw new NoSuchElementException();
+			} else
+			{
+				return iteratorArray[--length];
+			}
+		}
+
+		@Override
+		public void remove()
+		{
+			throw new UnsupportedOperationException();
+		}
 
 	}
 
+	/**
+	 * Resizes the array and matches the values with what was originally stored
+	 * after the modification.
+	 * 
+	 * @param newSize
+	 */
+	private void resize(int newSize)
+	{
+		@SuppressWarnings("unchecked")
+		Item[] newStorage = (Item[]) new Object[newSize];
+		for (int i = 0; i < n; i++)
+		{
+			newStorage[i] = itemArray[i];
+		}
+		itemArray = newStorage;
+	}
+
+	/**
+	 * Checks whether the array is full.
+	 * 
+	 * @return
+	 */
+	private boolean isStorageFull()
+	{
+		return itemArray.length == n;
+	}
+
+	/**
+	 * Doubles the size of the array.
+	 */
+	private void doubleSize()
+	{
+		resize(itemArray.length * 2);
+	}
+
+	/**
+	 * Checks whether the array is only using a quarter of the array size. This
+	 * validates if the array is larger than it needs to be.
+	 * 
+	 * @return
+	 */
+	private boolean isStorageOversized()
+	{
+		return itemArray.length > 2 && n <= itemArray.length / 4;
+	}
+
+	/**
+	 * Resizes the array to half of its' original length.
+	 */
+	private void halveSize()
+	{
+		resize(itemArray.length / 2);
+	}
+
+	/**
+	 * Unit Testing
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
-
+		// See J unit Tests
 	}
 
 }
