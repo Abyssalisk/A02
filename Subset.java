d31c4692fb1a21b0c47839d75a695fc6df72921d
Abyssalisk <Abyssalisk@gmail.com>
2018-09-29 19:21:16 -0600

    Added functionality and comments.

--- Subset.java
+++ Subset.java
@@ -1,5 +1,8 @@
 package a02;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 
  * @author Gavin Rosenvall & Khoi Tran
@@ -7,11 +10,24 @@ package a02;
  */
 public class Subset
 {
-
+	/**
+	 * Reads in the input from the user. K is parsed to the integer that the
+	 * user inputs. The rest of the strings are then added to an array and
+	 * randomly removed back out and printed back k times.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		int k = Integer.parseInt(args[0]);
+		RandomizedQueue<String> stringQueue = new RandomizedQueue<>();
 
+		for (String el : StdIn.readAllStrings())
+		{
+			stringQueue.enqueue(el);
+		}
+
+		for (int i = 0; i < k; i++)
+			StdOut.println(stringQueue.dequeue());
 	}
-
 }
